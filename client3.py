f87a988f5379dc40d7cdba91480cc9e406507bc4
anandspaces <anandvaibhaw000@gmail.com>
2024-06-18 21:00:46 +0530

    updated to meet requirements

--- client3.py
+++ client3.py
@@ -61,9 +61,5 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-        
-        if 'ABC' in prices and 'DEF' in prices:
-            ratio = getRatio(prices['ABC'], prices['DEF'])
-            print("Ratio ABC/DEF: %s" % (ratio))
 
-        # print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
